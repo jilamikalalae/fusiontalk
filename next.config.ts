9ed9734b209274ed53a2a0945fa1b41f3252e4bc
Guitar <jilamikalalae@gmail.com>
2024-11-27 01:39:05 +0700

    apply template

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
