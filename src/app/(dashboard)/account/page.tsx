8666d9242ed965b650c63c0fa1f05c138967f0b1
Guitar <jilamikalalae@gmail.com>
2024-12-11 20:08:52 +0700

    add modal get token

--- src/app/(dashboard)/account/page.tsx
+++ src/app/(dashboard)/account/page.tsx
@@ -1,5 +1,30 @@
 'use client';
 import React, { useState } from 'react';
+import LineConnect from '@/components/account/lineConnect';
+import {
+  Modal,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Link,
+  SxProps,
+  Theme
+} from '@mui/material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  borderRadius: '8px',
+  boxShadow: 24,
+  p: 4
+} as SxProps<Theme>;
 
 interface IntegratedAccount {
   id: number;
@@ -13,28 +38,31 @@ const integratedAccounts: IntegratedAccount[] = [
     id: 1,
     name: 'Line',
     description: 'Connect your line account.',
-    isConnected: false,
+    isConnected: false
   },
   {
     id: 2,
     name: 'Messenger',
     description: 'Connect your messenger account.',
-    isConnected: true,
-  },
+    isConnected: true
+  }
 ];
 
 const AccountManagementPage: React.FC = () => {
   const [notifications, setNotifications] = useState({
     email: true,
     sms: false,
-    app: true,
+    app: true
   });
   const [is2FAEnabled, setIs2FAEnabled] = useState(false);
 
-  const toggleNotification = (type: "email" | "sms" | "app") => {
-  setNotifications((prev) => ({ ...prev, [type]: !prev[type] }));
-};
+  const toggleNotification = (type: 'email' | 'sms' | 'app') => {
+    setNotifications((prev) => ({ ...prev, [type]: !prev[type] }));
+  };
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <div className="bg-white shadow-lg rounded-lg w-full h-full p-6 space-y-6">
@@ -122,22 +150,19 @@ const AccountManagementPage: React.FC = () => {
                 <p className="text-sm text-gray-500">{account.description}</p>
               </div>
               <button
-          className={`px-4 py-2 rounded-lg border-2 ${
-            account.isConnected
-              ? 'border-gray-300 text-gray-700 bg-white'
-              : 'border-green-500 text-green-500 bg-white'
-          }`}
-        >
+                onClick={handleOpen}
+                className={`px-4 py-2 rounded-lg border-2 ${
+                  account.isConnected
+                    ? 'border-gray-300 text-gray-700 bg-white'
+                    : 'border-green-500 text-green-500 bg-white'
+                }`}
+              >
                 {account.isConnected ? 'Connected' : 'Connect'}
               </button>
             </li>
           ))}
         </ul>
       </div>
-
-     
-
-     
 
       {/* Account Security */}
       <div>
@@ -151,6 +176,64 @@ const AccountManagementPage: React.FC = () => {
           </button>
         </div>
       </div>
+      <LineConnect requestOpen={open} onClose={handleClose}/>
+      {/* <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-title" variant="h6" component="h2">
+              Sign in to our platform
+            </Typography>
+            <Box component="form" sx={{ mt: 2 }}>
+              <TextField
+                fullWidth
+                label="Your email"
+                type="email"
+                variant="outlined"
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Your password"
+                type="password"
+                variant="outlined"
+                margin="normal"
+              />
+              <FormControlLabel control={<Checkbox />} label="Remember me" />
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  mt: 1
+                }}
+              >
+                <Link href="#" underline="hover">
+                  Lost Password?
+                </Link>
+              </Box>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+              >
+                Login to your account
+              </Button>
+              <Typography variant="body2" sx={{ textAlign: 'center', mt: 2 }}>
+                Not registered?{' '}
+                <Link href="#" underline="hover">
+                  Create account
+                </Link>
+              </Typography>
+            </Box>
+          </Box>
+        </Modal>
+      </div> */}
     </div>
   );
 };
