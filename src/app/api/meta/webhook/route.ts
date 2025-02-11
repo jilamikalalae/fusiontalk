2d46880610eaa57eda8daf1e1b4ec6ad67121643
Guitar <jilamikalalae@gmail.com>
2025-02-11 23:29:11 +0700

    Add Meta message webhook prototype

---
+++ src/app/api/meta/webhook/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const ACCESS_TOKEN = process.env.VERIFY_TOKEN;
+
+// API FOR VERIFY WEBHOOK API
+export async function GET(req: NextRequest) {
+    const challenge = req.nextUrl?.searchParams.get('hub.challenge')
+    return new Response(challenge)
+}
+
+// WEBHOOK Tutorial
+export async function POST(req: any) {
+    const body = await req.json(); 
+    const messaging = body.entry?.[0]?.messaging;
+    const senderId = messaging[0].sender.id
+    
+    console.log('Message Info: ',messaging)
+
+    const getProfileUrl = `https://graph.facebook.com/${senderId}?fields=first_name,last_name,profile_pic&access_token=${ACCESS_TOKEN}`;
+
+    const response = await fetch(getProfileUrl);
+    const data = await response.json();
+
+    console.log('Sender Profile',data);
+
+    return NextResponse.json(req)
+}
